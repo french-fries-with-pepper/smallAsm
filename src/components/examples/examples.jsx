1b1f5889e0e77d374f4a961d496e5c04e9770641
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2022-10-13 21:24:41 +0300

    feat: add examples page, with pre-loaded programs

--- src/components/examples/examples.jsx
+++ src/components/examples/examples.jsx
@@ -1,9 +1,76 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import "./examples.css";
+import { fib, fact, pow, gcd } from "./programs";
 
 function Examples() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const changeCode = (code) => {
+    dispatch({ type: "changed", payload: code });
+  };
   return (
-    <main>
-      <h2>Examples will be available soon</h2>
+    <main className="examples">
+      <h2 className="examples__header">Example of programs</h2>
+      <ul className="examples__list">
+        <li className="examples__item">
+          <h3 className="examples__name">Fibonacci</h3>
+          <p className="examples__desc">
+            This program takes n and returns nth fibonacci number.
+          </p>
+          <button
+            className="examples__runBtn"
+            onClick={() => {
+              changeCode(fib);
+              navigate("/");
+            }}
+          >
+            try
+          </button>
+        </li>
+        <li className="examples__item">
+          <h3 className="examples__name">Factorial</h3>
+          <p className="examples__desc">Retorns factorial of given number.</p>
+          <button
+            className="examples__runBtn"
+            onClick={() => {
+              changeCode(fact);
+              navigate("/");
+            }}
+          >
+            try
+          </button>
+        </li>
+        <li className="examples__item">
+          <h3 className="examples__name">Power</h3>
+          <p className="examples__desc">Take two numbers and returns a ^ b</p>
+          <button
+            className="examples__runBtn"
+            onClick={() => {
+              changeCode(pow);
+              navigate("/");
+            }}
+          >
+            try
+          </button>
+        </li>
+        <li className="examples__item">
+          <h3 className="examples__name">GCD</h3>
+          <p className="examples__desc">
+            Take two numbers and returns it's Gratest Common Divisor
+          </p>
+          <button
+            className="examples__runBtn"
+            onClick={() => {
+              changeCode(gcd);
+              navigate("/");
+            }}
+          >
+            try
+          </button>
+        </li>
+      </ul>
     </main>
   );
 }
---
+++ src/components/examples/programs.js
@@ -0,0 +1,115 @@
+export const fib = `mov   a, 8            ; value
+mov   b, 0            ; next
+mov   c, 0            ; counter
+mov   d, 0            ; first
+mov   e, 1            ; second
+call  proc_fib
+call  print
+end
+
+proc_fib:
+    cmp   c, 2
+    jl    func_0
+    mov   b, d
+    add   b, e
+    mov   d, e
+    mov   e, b
+    inc   c
+    cmp   c, a
+    jle   proc_fib
+    ret
+
+func_0:
+    mov   b, c
+    inc   c
+    jmp   proc_fib
+
+print:
+    msg   'Term ', a, ' of Fibonacci series is: ', b        ; output text
+    ret`;
+
+export const fact = `mov   a, 5         ;value
+mov   b, a
+mov   c, a
+call  proc_fact
+call  print
+end
+
+proc_fact:
+    dec   b
+    mul   c, b
+    cmp   b, 1
+    jne   proc_fact
+    ret
+
+print:
+    msg   a, '! = ', c ; output text
+    ret`;
+
+export const gcd = `mov   a, 81         ; value1
+mov   b, 153        ; value2
+call  init
+call  proc_gcd
+call  print
+end
+
+proc_gcd:
+    cmp   c, d
+    jne   loop
+    ret
+
+loop:
+    cmp   c, d
+    jg    a_bigger
+    jmp   b_bigger
+
+a_bigger:
+    sub   c, d
+    jmp   proc_gcd
+
+b_bigger:
+    sub   d, c
+    jmp   proc_gcd
+
+init:
+    cmp   a, 0
+    jl    a_abs
+    cmp   b, 0
+    jl    b_abs
+    mov   c, a            ; temp1
+    mov   d, b            ; temp2
+    ret
+
+a_abs:
+    mul   a, -1
+    jmp   init
+
+b_abs:
+    mul   b, -1
+    jmp   init
+
+print:
+    msg   'gcd(', a, ', ', b, ') = ', c
+    ret`;
+
+export const pow = `mov   a, 2            ; value1
+mov   b, 10           ; value2
+mov   c, a            ; temp1
+mov   d, b            ; temp2
+call  proc_func
+call  print
+end
+
+proc_func:
+    cmp   d, 1
+    je    continue
+    mul   c, a
+    dec   d
+    call  proc_func
+
+continue:
+    ret
+
+print:
+    msg a, '^', b, ' = ', c
+    ret`;
